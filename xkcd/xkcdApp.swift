0e90aaaaa9e63c188a60d095ed0013f3fb607cb8
Sylvan Martin <slm338@cornell.edu>
2025-10-09 20:34:43 -0400

    Initial Commit

---
+++ xkcd/xkcdApp.swift
@@ -0,0 +1,32 @@
+//
+//  xkcdApp.swift
+//  xkcd
+//
+//  Created by Sylvan Martin on 10/9/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct xkcdApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ xkcdTests/xkcdTests.swift
@@ -0,0 +1,17 @@
+//
+//  xkcdTests.swift
+//  xkcdTests
+//
+//  Created by Sylvan Martin on 10/9/25.
+//
+
+import Testing
+@testable import xkcd
+
+struct xkcdTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
